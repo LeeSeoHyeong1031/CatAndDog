1c07581217a21ef89eb6e8a21eb3f1dfe25beb00
LeeSeoHyeong1031 <champ36912@naver.com>
2024-10-31 01:22:04 +0900

    README.md 업뎃

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,20 +4,63 @@ using UnityEngine;
 
 public class Enemy : Unit
 {
-    private void Update()
-    {
-        //적이 있다면 if문 실행
-        if (isTarget)
-        {
-            //공격이 가능하다면 공격.
-            if (CanAttack()) Attack();
-        }
-    }
-    private void FixedUpdate()
-    {
-        //타겟이 없다면 움직이기
-        if (isTarget == false) Move(Vector2.right);
+	public Animator anim;
 
-        TargetScan(Vector2.right);
-    }
+	private void Start()
+	{
+		LookDir();
+	}
+	private void Update()
+	{
+		if (dead) { return; }
+
+		//적이 있다면 if문 실행
+		if (isTarget)
+		{
+			//공격이 가능하다면 공격 거리 안에 있는 colls 모두 Attack().
+			if (CanAttack())
+			{
+				Attack();
+				anim.SetTrigger("2_Attack");
+			}
+		}
+	}
+	private void FixedUpdate()
+	{
+		if (dead) { return; }
+
+		//타겟이 없다면 움직이기
+		if (isTarget == false)
+		{
+			Move(Vector2.right);
+			anim.SetBool("1_Move", true);
+		}
+		else anim.SetBool("1_Move", false);
+
+		//적을 감지하고 감지가 되면 isTarget = true
+		TargetScan(Vector2.right);
+	}
+
+	public override void TakeDamage(int damage)
+	{
+		base.TakeDamage(damage);
+		//anim.SetTrigger("3_Damaged");
+		if (health <= 0)
+		{
+			Die();
+		}
+	}
+
+	public override void Die()
+	{
+		base.Die();
+		GetComponent<CircleCollider2D>().enabled = false;
+		anim.SetBool("isDeath", true);
+		anim.SetTrigger("4_Death");
+	}
+
+	public void LookDir()
+	{
+		transform.localScale = new Vector3(-10, 10, 10);
+	}
 }
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -6,107 +6,108 @@ using UnityEngine;
 //모든 유닛의 기본 속성, 메소드
 public class Unit : MonoBehaviour, ITakeDamage
 {
-    [Tooltip("유닛 스텟 데이터 입니다.")]
-    [SerializeField] private UnitData unitData;
-    //takedamage
-    internal int health; //체력
+	[Tooltip("유닛 스텟 데이터 입니다.")]
+	[SerializeField] private UnitData unitData;
+	//takedamage
+	internal int health; //체력
 
 
-    // move
-    internal int moveSpeed; //이동 속도
+	// move
+	internal int moveSpeed; //이동 속도
 
-    //attack
-    internal int damage; //공격력
-    internal Vector2Int attackRange; //공격 사정 거리
-    internal float attackInterval; //공격속도
-    internal float lastAttackTime = 0; //마지막 공격 시간
-    internal bool isTarget = false; //적이 감지되면 멈추기
-    public LayerMask targetLayer; //타겟 레이어
+	//attack
+	internal int damage; //공격력
+	internal Vector2Int attackRange; //공격 사정 거리
+	internal float attackInterval; //공격속도
+	internal float lastAttackTime = 0; //마지막 공격 시간
+	internal bool isTarget = false; //적이 감지되면 멈추기
+	public LayerMask targetLayer; //타겟 레이어
 
-    // spawn
-    internal int cost; //생산 비용
+	// spawn
+	internal int cost; //생산 비용
 
-    private Rigidbody2D rb;
-    protected Collider2D[] colls;
-    private Vector2 boxCenter;
-    private Vector2 boxSize;
+	internal bool dead; //플레이어 죽음
+	private Rigidbody2D rb;
+	protected Collider2D[] colls;
+	private Vector2 boxCenter;
+	private Vector2 boxSize;
 
-    protected void Awake()
-    {
-        Init();
-    }
+	protected void Awake()
+	{
+		Init();
+	}
 
-    //데미지 받는 메서드
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
-        print($"{this.name} 아야!");
-        if (health <= 0)
-        {
-            //죽었을 때 처리
-            GetComponent<CircleCollider2D>().enabled = false;
-            this.gameObject.SetActive(false); //임시
-        }
-    }
+	//데미지 받는 메서드
+	public virtual void TakeDamage(int damage)
+	{
+		health -= damage;
+		print($"{this.name}의 남은 체력 : {health}");
+	}
 
-    //적 감지 메서드
-    public void TargetScan(Vector2 dir)
-    {
-        boxCenter = (Vector2)transform.position + dir * 4; // 박스 Pivot 왼쪽으로 4만큼 이동
-        colls = Physics2D.OverlapBoxAll(boxCenter, boxSize, 0, targetLayer);
-        if (colls.Length > 0) isTarget = true;
-        else isTarget = false;
-    }
+	public virtual void Die()
+	{
+		health = 0;
+		dead = true;
+	}
 
-    //공격 할 수 있는지
-    public bool CanAttack()
-    {
-        if (Time.time >= lastAttackTime)
-        {
-            lastAttackTime = Time.time + attackInterval;
-            return true;
-        }
-        else return false;
-    }
+	//적 감지 메서드
+	public void TargetScan(Vector2 dir)
+	{
+		boxCenter = (Vector2)transform.position + dir * 4; // 박스 Pivot 왼쪽으로 4만큼 이동
+		colls = Physics2D.OverlapBoxAll(boxCenter, boxSize, 0, targetLayer);
+		if (colls.Length > 0) isTarget = true;
+		else isTarget = false;
+	}
 
-    //공격 메서드
-    public void Attack()
-    {
-        foreach (Collider2D collider in colls)
-        {
-            if (collider.TryGetComponent<ITakeDamage>(out ITakeDamage other))
-            {
-                other.TakeDamage(damage);
-            }
-        }
-    }
+	//공격 할 수 있는지
+	public bool CanAttack()
+	{
+		if (Time.time >= lastAttackTime)
+		{
+			lastAttackTime = Time.time + attackInterval;
+			return true;
+		}
+		else return false;
+	}
 
-    //이동 메서드
-    public void Move(Vector2 dir)
-    {
-        rb.MovePosition(rb.position + (moveSpeed * dir * Time.fixedDeltaTime));
-    }
+	//공격 메서드
+	public void Attack()
+	{
+		foreach (Collider2D collider in colls)
+		{
+			if (collider.TryGetComponent<ITakeDamage>(out ITakeDamage other))
+			{
+				other.TakeDamage(damage);
+			}
+		}
+	}
 
-    //모든 초기화 담당. //추후에 세분화 가능
-    public void Init()
-    {
-        //스텟 초기화
-        health = unitData.health;
-        damage = unitData.damage;
-        attackInterval = unitData.attackInterval;
-        attackRange = unitData.attackRange;
-        moveSpeed = unitData.moveSpeed;
-        cost = unitData.cost;
-        //컴포넌트 할당
-        rb = GetComponent<Rigidbody2D>();
-        //BoxCenter,BoxSize 초기화
-        boxSize.x = attackRange.x;
-        boxSize.y = attackRange.y;
-    }
+	//이동 메서드
+	public void Move(Vector2 dir)
+	{
+		rb.MovePosition(rb.position + (moveSpeed * dir * Time.fixedDeltaTime));
+	}
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireCube(boxCenter, boxSize);
-    }
+	//모든 초기화 담당. //추후에 세분화 가능
+	public void Init()
+	{
+		//스텟 초기화
+		health = unitData.health;
+		damage = unitData.damage;
+		attackInterval = unitData.attackInterval;
+		attackRange = unitData.attackRange;
+		moveSpeed = unitData.moveSpeed;
+		cost = unitData.cost;
+		//컴포넌트 할당
+		rb = GetComponent<Rigidbody2D>();
+		//BoxCenter,BoxSize 초기화
+		boxSize.x = attackRange.x;
+		boxSize.y = attackRange.y;
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube(boxCenter, boxSize);
+	}
 }
